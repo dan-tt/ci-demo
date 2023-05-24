6f0113c3c2600ea869e618079d1c1d0fb0e36993
Dan Tran <dan.tt@amanotes.com>
2023-05-24 20:52:06 +0700

    add xcode project

---
+++ CIDemo/CIDemo/CIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIDemoApp.swift
+//  CIDemo
+//
+//  Created by Dan Tran on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
